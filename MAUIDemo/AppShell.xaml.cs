d20751a20618aebf447f48ad9057ae3289bbfc0e
Jesse Smith <JesseMSmith@gmail.com>
2022-09-27 11:07:26 -0400

    Added bottom bar icons

--- MAUIDemo/AppShell.xaml.cs
+++ MAUIDemo/AppShell.xaml.cs
@@ -9,6 +9,10 @@ public partial class AppShell : Shell
 		InitializeComponent();
 
         Routing.RegisterRoute("article", typeof(ArticlePage));
+
+		this.HomeTab.Icon = ImageSource.FromResource("MAUIDemo.Resources.Images.home.png", this.GetType().Assembly);
+        this.SectionsTab.Icon = ImageSource.FromResource("MAUIDemo.Resources.Images.categories.png", this.GetType().Assembly);
+        this.BookmarksTab.Icon = ImageSource.FromResource("MAUIDemo.Resources.Images.bookmarks.png", this.GetType().Assembly);
+        this.ProfileTab.Icon = ImageSource.FromResource("MAUIDemo.Resources.Images.profile.png", this.GetType().Assembly);
     }
 }
-
